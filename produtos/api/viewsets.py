9cb5c217a57dafa69e403bf4d52f69c9dc7b8212
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-28 11:12:50 -0300

    Add filtro por codigo e descricao no produto

--- produtos/api/viewsets.py
+++ produtos/api/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from produtos.models import Produto
 from .serializers import ProdutoSerializer
 
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('cod_produto', 'descricao')
