4eb76caf12c8032e9534f46791b368328e2f8389
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-28 04:00:52 -0300

    Add action para relacionar produtos a loja

--- lojas/api/viewsets.py
+++ lojas/api/viewsets.py
@@ -1,4 +1,8 @@
+from crypt import methods
+
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from lojas.models import Loja
 from .serializers import LojaSerializer
@@ -6,3 +10,11 @@ from .serializers import LojaSerializer
 class LojaViewSet(ModelViewSet):
     queryset = Loja.objects.all()
     serializer_class = LojaSerializer
+
+    @action(methods=['post'], detail=True)
+    def associa_produtos(self, request, pk):
+        produtos = request.data['ids']
+        loja = Loja.objects.get(pk=pk)
+        loja.produtos.set(produtos)
+        loja.save()
+        return HttpResponse('Ok')
