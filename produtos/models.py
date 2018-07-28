b29c9e8ae3b7864bf41967f1edc730a1518856b0
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-28 00:42:43 -0300

    Add model Produto
    Campos para cadastro de produto
    cod_produto -> unique
    descricao -> textField
    valor_venda -> decimal maximo 7 digitos e 2 casas decimais

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    cod_produto = models.CharField(max_length=15, unique=True)
+    descricao = models.TextField()
+    valor_venda = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.cod_produto + ' - ' + self.descricao
