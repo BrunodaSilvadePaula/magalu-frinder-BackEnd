f98105a2a367b47a5fbbe6b18f988b495b45517f
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-28 00:04:54 -0300

    Add model loja

--- lojas/models.py
+++ lojas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Loja(models.Model):
+    descricao = models.TextField()
+    cod_filial = models.IntegerField(null=True, blank=True)
+    cep = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'Filial - ' + str(self.cod_filial)
