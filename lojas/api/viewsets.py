35dfdd2560789db536eba060e7528de120011c7d
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-28 11:11:50 -0300

    Add busca por produtos nas lojas

--- lojas/api/viewsets.py
+++ lojas/api/viewsets.py
@@ -2,6 +2,7 @@ from crypt import methods
 
 from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from lojas.models import Loja
@@ -10,6 +11,8 @@ from .serializers import LojaSerializer
 class LojaViewSet(ModelViewSet):
     queryset = Loja.objects.all()
     serializer_class = LojaSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('produtos__cod_produto', 'produtos__descricao')
 
     @action(methods=['post'], detail=True)
     def associa_produtos(self, request, pk):
