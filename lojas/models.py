539e5ecf89dca242229d04f906f34a66eb48e0b2
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-28 00:47:23 -0300

    Add relacionamento produto e unique cod_filial
    Transformando o campo cod_filial em unique
    Adicionando relacionamento many to many com a class Produto

--- lojas/models.py
+++ lojas/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from produtos.models import Produto
 
 # Create your models here.
 
 class Loja(models.Model):
     descricao = models.TextField()
-    cod_filial = models.IntegerField(null=False, blank=False)
+    cod_filial = models.IntegerField(null=False, blank=False, unique=True)
     cep = models.CharField(max_length=10)
+    produtos = models.ManyToManyField(Produto, blank=True, null=True)
 
     def __str__(self):
         return 'Filial - ' + str(self.cod_filial)
