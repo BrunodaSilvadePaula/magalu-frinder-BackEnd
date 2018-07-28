a16fb9f6ed2901d9bf1249da713f2f6932b59dc8
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-28 04:00:08 -0300

    Add produtos ao serializer da loja

--- lojas/api/serializers.py
+++ lojas/api/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 from lojas.models import Loja
+from produtos.api.serializers import ProdutoSerializer
 
 class LojaSerializer(ModelSerializer):
+    produtos = ProdutoSerializer(many=True, read_only=True)
+
     class Meta:
         model = Loja
-        fields = ('id', 'descricao', 'cod_filial', 'cep')
+        fields = ('id', 'descricao', 'cod_filial', 'cep', 'produtos')
