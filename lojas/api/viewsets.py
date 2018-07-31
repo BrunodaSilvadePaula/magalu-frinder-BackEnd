e454b2424701630e2acd8f7d8868e2bb1af074ad
brunodasilvadepaula <bruno@bettacom.com.br>
2018-07-31 17:44:43 -0300

    Add ordenacao na busca de produto, lojas proxima

--- lojas/api/viewsets.py
+++ lojas/api/viewsets.py
@@ -6,6 +6,7 @@ from rest_framework.filters import SearchFilter
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+from operator import itemgetter
 import urllib.request, json
 from lojas.models import Loja
 from produtos.models import Produto
@@ -78,4 +79,5 @@ class LojaViewSet(ModelViewSet):
                 "distancia": self.get_distancia(cep, filial.cep)
             }
             retorno.append(obj)
+        retorno = sorted(retorno, key =lambda x: x['distancia'])
         return retorno
