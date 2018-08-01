c4cb54e90dbf5c5a8541021f5e3a1de4588a037f
brunodasilvadepaula <bruno@bettacom.com.br>
2018-08-01 19:28:16 -0300

    Add configuracoes para o banco

--- magalu_finder/settings.py
+++ magalu_finder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,14 +82,17 @@ WSGI_APPLICATION = 'magalu_finder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR,'my.cnf')
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': os.path.join(BASE_DIR,'my.cnf')
+#         }
+#     }
+# }
+default_dburl = 'read_default_file:' + os.path.join(BASE_DIR,'my.cnf')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl),  }
 
 
 # Password validation
