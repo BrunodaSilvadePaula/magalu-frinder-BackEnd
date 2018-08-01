408b9af99478dc725b4ffd5e4627ba26a229d94f
brunodasilvadepaula <bruno@bettacom.com.br>
2018-08-01 19:14:00 -0300

    Configurando app

---
+++ README.md
@@ -0,0 +1,52 @@
+# Back-End Magalu-Finder
+
+---
+## Preparando o ambiente no linux (ubuntu)
+
+* Instale o python 3.6
+
+* Instale o pip
+
+* Instale as dependêcias do python para dev
+
+* Instale as libs do mysql
+
+* Instale a virtualenv
+
+## Instalação
+
+```sh
+
+$ sudo apt-get update
+
+$ sudo apt-get install python3.6
+
+$ sudo apt-get install python3-pip
+
+$ sudo apt-get install python3.6-dev
+
+$ sudo apt-get install libmysqlclient-dev
+
+$ sudo apt-get install python3.6-venv
+```
+
+## Com a instalação feita
+
+```sh
+
+* Na sua pasta de projetos crie a sua virtualenv
+
+$ python3.6 -m venv [nome da sua venv]
+
+* Abra sua venv e clone o repositorio
+
+
+* Entre no repositorio e instele as dependências
+
+$ 
+```
+
+
+
+install sudo apt-get install python3.6-dev libmysqlclient-dev
+pip3 install mysqlclient
--- magalu_finder/settings.py
+++ magalu_finder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '94y@lz&7y=^5s9f06@dc93w5paug&5(m4515r#&!833i54mj+t'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost:3000', 'localhost:8000', 'localhost']
+ALLOWED_HOSTS = ['localhost:3000', 'localhost:8000', 'localhost','magalu-finder-bruno.herokuapp.com']
 
 
 # Application definition
@@ -143,6 +144,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
